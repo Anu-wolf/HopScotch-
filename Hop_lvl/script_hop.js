17381903be7e2c646daad96c834bec60ee18bc3a
Anu-Wolf <anushri.2335@gmail.com>
2025-05-23 13:46:00 +0530

    transform_10

--- Hop_lvl/script_hop.js
+++ Hop_lvl/script_hop.js
@@ -119,19 +119,19 @@ class HopscotchGame {
 
     switch (buttonId) {
       case 'Hop':
-        moveDistance = { y: 50, x: 0 };
+        moveDistance = { y: 30, x: 0 };
         break;
       case 'Skip':
-        moveDistance = { y: 50, x: 0 };
+        moveDistance = { y: 100, x: 0 };
         break;
       case 'Jump':
-        moveDistance = { y: 50, x: 0 };
+        moveDistance = { y: 30, x: 0 };
         break;
       case 'Skip-HopRight':
-        moveDistance = { y: 50, x: 32 };
+        moveDistance = { y: 30, x: 32 };
         break;
       case 'Skip-HopLeft':
-        moveDistance = { y: 50, x: -32 };
+        moveDistance = { y: 30, x: -32 };
         break;
       default:
         moveDistance = { y: 0, x: 0 };
